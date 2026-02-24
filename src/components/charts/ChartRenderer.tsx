3e2defc074fcc925e04284e80b889503e038f383
douglaslopesfreitas <douglaslopes.freitas21@gmailcom>
2026-02-24 14:19:36 -0300

    oi

--- src/components/charts/ChartRenderer.tsx
+++ src/components/charts/ChartRenderer.tsx
@@ -109,7 +109,7 @@ function SimpleTooltip({
 })()}
 
 {/* 📈 CASO LINHA (EVOLUÇÃO) */}
-{payload.length === 1 && typeof total !== "number" && label && (() => {
+{{payload.length === 1 && typeof total !== "number" && !label && (() => {
   const p = payload[0]
   const value = Number(p?.value ?? 0)
 
